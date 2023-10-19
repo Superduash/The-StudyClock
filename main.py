407e70d8894a40e7ffd9a24f40da5c1bade79cff
Superduash <aashwinsuperdu@gmail.com>
2023-10-19 23:06:54 +0530

    huh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+
 app = QApplication([])
 window = QMainWindow()
 window.setWindowTitle("The StudyClock")
@@ -12,3 +13,17 @@ layout.addWidget(QPushButton("Settings"))
 cw.setLayout(layout)
 window.show()
 
+class StudyClockApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        # Your main window initialization code will go here
+        self.setWindowTitle("StudyClock")
+        self.setGeometry(100, 100, 800, 600)
+        self.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = StudyClockApp()
+    sys.exit(app.exec_())
