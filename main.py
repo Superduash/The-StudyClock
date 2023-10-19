a0501dcb96ff18f355dc737016082960a1b3d218
Superduash <aashwinsuperdu@gmail.com>
2023-10-19 22:22:29 +0530

    1

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 app = QApplication([])
 window = QMainWindow()
 window.setWindowTitle("The StudyClock")
+cw = QWidget()
+window.setCentralWidget(cw)
+layout = QVBoxLayout()
+layout.addWidget(QLabel("Welcome to The StudyClock!!"))
+layout.addWidget(QPushButton("Start Timer"))
+layout.addWidget(QPushButton("Settings"))
+cw.setLayout(layout)
+window.show()
+
