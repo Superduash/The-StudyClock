43167edaca9759515cb2a861f20448f8e156cd20
Superduash <aashwinsuperdu@gmail.com>
2023-10-20 10:15:03 +0530

    gg

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
-
-app = QApplication(sys.argv)
-
-class StudyClockApp(QMainWindow):
+from PyQt5.QtCore import Qt
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("The StudyClock")
+        self.setWindowState(Qt.WindowMaximized)
 
-        self.setGeometry(100, 100, 800, 600)
-        self.setWindowTitle("Study Clock")
-        self.initUI()
-
-    def initUI(self):
-        label = QLabel("Hello, Study Clock!", self)
-        label.move(50, 50)
-
-        button = QPushButton("Click Me", self)
-        button.move(50, 100)
-        button.clicked.connect(self.on_button_click)
-
-    def on_button_click(self):
-        print("Button clicked!")
-
-main_window = StudyClockApp()
-main_window.show()
-
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
