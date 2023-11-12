d4915478bf866a9297275f4bfbbcdd1b14d5c08d
Superduash <aashwinsuperdu@gmail.com>
2023-11-12 08:47:55 +0530

    huh

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ class MyWindow(QMainWindow):
         desktop = QDesktopWidget()
         screen_rect = desktop.availableGeometry()
         initial_width = screen_rect.width() * 1
-        initial_height = screen_rect.height() * 1
-
+        initial_height = screen_rect.height() * 0.966
         self.setWindowTitle("The StudyClock")
+        self.setGeometry((screen_rect.width() - initial_width) / 2, (screen_rect.height() - initial_height) / 2, initial_width, initial_height)
+        self.setWindowFlags(Qt.WindowTitleHint | Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
         self.setWindowState(Qt.WindowMaximized)
-        self.setFixedSize(initial_width, initial_height)
+        self.setFixedSize(self.size())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
+    icon_path = "path/to/your/icon.png"
+    self.setWindowIcon(QIcon(icon_path))
