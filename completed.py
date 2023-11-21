f7896f53f4652123b4511b1cdc88b5ec7653260c
AnomHead <anomhead2022@gmail.com>
2023-11-21 06:59:45 +0530

    changes

--- completed.py
+++ completed.py
@@ -2,24 +2,19 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from datetime import timedelta, datetime
-from plyer import notification
+from datetime import datetime, timedelta
 from PyQt5.QtMultimedia import QSoundEffect
-import mysql.connector
-from todolistgui import Ui_MainWindow
+from completedtasksui import Ui_MainWindow
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+
+        # Connect signals and slots or perform additional setup here
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
     window.show()
     sys.exit(app.exec_())
-
-
-
-
-
